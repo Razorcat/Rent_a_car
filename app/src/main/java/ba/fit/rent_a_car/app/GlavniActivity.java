fe4be47b69fa13e8a8155603958de58d3ba69b93
Salih <salih.sabanovic@edu.fit.ba>
2014-08-22 16:44:08 +0200

    Ispravljanje grešaka

--- app/src/main/java/ba/fit/rent_a_car/app/GlavniActivity.java
+++ app/src/main/java/ba/fit/rent_a_car/app/GlavniActivity.java
@@ -7,6 +7,7 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
@@ -30,7 +31,7 @@ public class GlavniActivity extends Activity {
     private CharSequence NavDrawerTitle;
     private CharSequence AppTitle;
 
-    private  String[] NavDrawerItemTitles;
+    private String[] NavDrawerItemTitles;
     private TypedArray NavDrawerIcons;
 
     private ArrayList<NavigationDrawerItem> NavDrawerItems;
@@ -46,7 +47,7 @@ public class GlavniActivity extends Activity {
         this.NavDrawerTitle = this.getTitle();
 
         this.NavDrawerItemTitles = this.getResources().getStringArray(R.array.nav_drawer_strings);
-        this.NavDrawerIcons = this.getResources().getStringArray(R.array.nav_drawer_icons);
+        this.NavDrawerIcons = this.getResources().obtainTypedArray(R.array.nav_drawer_icons);
 
         this.NavDrawerItems.add(new NavigationDrawerItem(NavDrawerItemTitles[0], NavDrawerIcons.getResourceId(0, -1)));
         this.NavDrawerItems.add(new NavigationDrawerItem(NavDrawerItemTitles[1], NavDrawerIcons.getResourceId(1, -1)));
@@ -60,41 +61,47 @@ public class GlavniActivity extends Activity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 DisplayFragment(i);
                 setTitle(NavDrawerItemTitles[i]);
-                NavDraverListView.setItemChecked(i,true);
+                NavDraverListView.setItemChecked(i, true);
                 NavDrawerLayout.closeDrawer(NavDraverListView);
 
             }
         });
 
-        this.NavDrawerAdapter = new NavigationDrawerListAdapter(this.getApplicationContext(),NavDrawerItems);
+        this.NavDrawerAdapter = new NavigationDrawerListAdapter(this.getApplicationContext(), NavDrawerItems);
         this.NavDraverListView.setAdapter(NavDrawerAdapter);
 
         this.getActionBar().setDisplayHomeAsUpEnabled(true);
         this.getActionBar().setHomeButtonEnabled(true);
-        this.NavDrawerToogle= new ActionBarDrawerToggle(this.getApplicationContext(),this.NavDrawerLayout,R.drawable.ic_navigation_drawer, R.string.app_name,R.string.app_name){
+        this.NavDrawerToogle = new ActionBarDrawerToggle(this,this.NavDrawerLayout, R.drawable.ic_navigation_drawer, R.string.app_name, R.string.app_name) {
             @Override
             public void onDrawerClosed(View drawerView) {
-               getActionBar().setTitle(NavDrawerTitle);
+                getActionBar().setTitle(NavDrawerTitle);
                 invalidateOptionsMenu();
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
-               getActionBar().setTitle(AppTitle);
+                getActionBar().setTitle(AppTitle);
                 invalidateOptionsMenu();
             }
         };
+
+        this.NavDrawerLayout.setDrawerListener(this.NavDrawerToogle);
+
+        if (savedInstanceState == null) {
+            DisplayFragment(0);
+        }
     }
 
 
-    private void DisplayFragment(int position){
+    private void DisplayFragment(int position) {
         Fragment fragment = null;
 
-        switch (position){
+        switch (position) {
 
             case 0:
                 fragment = new PregledVozila_Fragment();
-            break;
+                break;
 
             case 1:
                 fragment = new RezervacijaVozila_Fragment();
@@ -105,6 +112,12 @@ public class GlavniActivity extends Activity {
 
             default:
                 break;
+
+            if (fragment != null)
+                this.getFragmentManager().beginTransaction().replace(R.id.frame_container, fragment).commit();
+            else
+                Log.e("24 hours rent", "Unable to create fragment !");
+
         }
 
     }
@@ -115,25 +128,51 @@ public class GlavniActivity extends Activity {
 //        txtDobrodosao.setText("Dobrodosli :" + strUsername);
 
 
-
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.action_settings).setVisible(this.NavDrawerLayout.isDrawerOpen(this.NavDraverListView));
+        return super.onPrepareOptionsMenu(menu);
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.glavni, menu);
-        return true;
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.NavDrawerToogle.syncState();
     }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        this.getActionBar().setTitle(title);
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.glavni, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (this.NavDrawerToogle.onOptionsItemSelected(item))
             return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
         }
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -143,9 +182,11 @@ public class GlavniActivity extends Activity {
     }
     */
     public void btnLogin_onClick(View view) { //mora biti public i primati view,
-        Intent intent = new Intent(this,PregledVozilaActivity.class);
+        Intent intent = new Intent(this, PregledVozilaActivity.class);
         startActivity(intent);
 
 
     }
 }
+
+
